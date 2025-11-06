5c52dd36397c9b3e5e35d6885eb32e3f68694735
Caleb Lowe <caleblowe@proton.me>
2025-11-06 14:01:41 -0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,7 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "$ENV_NAME"
 
 
-python -m pip install --no-cache-dir --index-url https://download.pytorch.org/whl/cu128 \
-
-torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1
+python -m pip install --no-cache-dir --index-url https://download.pytorch.org/whl/cu128 torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1
   
 if [ ! -d "models/Evo2/.git" ]; then
     git clone https://github.com/ArcInstitute/evo2 models/Evo2
@@ -22,7 +20,6 @@ fi
 pip install -e models/Evo2
 
 conda install -y -c conda-forge nccl
-  
 
 pip3 install --no-build-isolation transformer_engine[pytorch]
 
