1f190aa44406dd918321f58f5cad628fa0b48e46
Caleb Lowe <caleblowe@proton.me>
2025-11-06 13:46:28 -0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,32 @@
-conda create -n evo2 python=3.12
+#!/bin/bash
 
-cd evo2/
-pip install -e .
+set -e
+read -p "Conda env name [Press 'enter' for default: capstoneCS46X]: " ENV_NAME
+ENV_NAME=${ENV_NAME:-capstoneCS46X}
+  
+conda create -y -n "$ENV_NAME" python=3.12
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate "$ENV_NAME"
 
-module load cuda/12.8
-module load cudnn/8.9_cuda12
 
-conda install -c conda-forge nccl
+python -m pip install --no-cache-dir --index-url https://download.pytorch.org/whl/cu128 \
+
+torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1
+  
+if [ ! -d "models/Evo2/.git" ]; then
+    git clone https://github.com/ArcInstitute/evo2 models/Evo2
+else
+    echo "Directory models/Evo2 already exists, skipping git clone."
+fi
+
+pip install -e models/Evo2
+
+conda install -y -c conda-forge nccl
+  
 
 pip3 install --no-build-isolation transformer_engine[pytorch]
 
-pip install flash-attn==2.7.3
+pip install --no-cache-dir flash-attn
 
-pip install -r requirements.txt
+pip install biopython bcbio-gff omegaconf pandas
+
